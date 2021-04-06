f917e0a153f5e7fb64fc257a4fa12b2d33a21ddd
CristianOu <mccrys_00@yahoo.com>
2021-04-06 12:53:01 +0300

    redirect-uri

--- spotify_api/credentials.py
+++ spotify_api/credentials.py
@@ -1,5 +1,5 @@
 CLIENT_ID = "f9819aef8b014de5b46bef41c5ff0ab1"
 CLIENT_SECRET = "9bc06ea60bdc44e4a3e6547e415a9a0e"
-REDIRECT_URI = "django-react-music-controller.herokuapp.com/spotify_api/redirect"
+REDIRECT_URI = "https://django-react-music-controller.herokuapp.com/spotify_api/redirect"
 
 
